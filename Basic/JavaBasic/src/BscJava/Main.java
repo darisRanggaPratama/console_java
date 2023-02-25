23aa0fea5177f5b6e7f1f7a1294c806413e7cc1a
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-02-25 16:52:50 +0700

    initial commit

--- Basic/JavaBasic/src/BscJava/Main.java
+++ Basic/JavaBasic/src/BscJava/Main.java
@@ -1,40 +1,61 @@
 package BscJava;
 
 public class Main {
-    private static Double panjang, lebar, sisi, luas, volume;
 
     public static void main(String[] args) {
         // Method. Void Return
         luasPersegiPanjang();
+
         // Method. Value Return
         System.out.println(volumeKubus());
+
         // Function (Method has parameter and return)
         System.out.println(VolumeBalok(7.0, 8.0, 9.0));
+
         // Function (Method has parameter and void return)
         luasPersegi(7.0);
+
+        // Overloading
+        System.out.println(persegiPanjang(2, 5));
+        System.out.println(persegiPanjang(2, 5, 10));
+
     }
 
     private static void luasPersegiPanjang() {
-        panjang = 9.0;
-        lebar = 8.0;
-        luas = panjang * lebar;
+        double panjang = 9.0;
+        double lebar = 8.0;
+        double luas = panjang * lebar;
         System.out.println("\nLuas Persegi Panjang:\nPanjang: " + panjang + " x Lebar: " + lebar + " = " + luas);
     }
 
     private static String volumeKubus() {
-        sisi = 9.0;
-        volume = sisi * sisi * sisi;
+        double sisi = 9.0;
+        double volume = sisi * sisi * sisi;
         return "\nVolume Kubus. " + sisi + "^3 = " + volume;
     }
 
     private static Double VolumeBalok(Double panjang, Double lebar, Double tinggi) {
-        volume = panjang * lebar * tinggi;
+        double volume = panjang * lebar * tinggi;
         System.out.print("\nVolume Balok. \nPanjang: " + panjang + " x Lebar: " + lebar + " x Tinggi: " + tinggi + " = ");
         return volume;
     }
 
     private static void luasPersegi(Double sisi) {
-        luas = sisi * sisi;
+        double luas = sisi * sisi;
         System.out.println("\nLuas Persegi:\n Sisi: " + sisi + "^2 = " + luas);
+    }
+
+    private static String persegiPanjang(int panjang, int lebar)
+    {
+        int persegiPanjang = panjang * lebar;
+
+        return  "\nLuas Persegi Panjang.\nPanjang: " + panjang + " Lebar: " + lebar + " itu " + persegiPanjang;
+    }
+
+    private static String persegiPanjang(double panjang, double lebar, double tinggi)
+    {
+        double balok = panjang * lebar * tinggi;
+
+        return"\nVolume Balok.\nPanjang: " + panjang + " Lebar: " + lebar + " Tinggi: " + tinggi + " itu " + balok;
     }
 }
